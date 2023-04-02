06c28fca584698d3bc02635f56a35199e9535866
Sylvain <sylvain.joyeux@tidewise.io>
2023-04-02 20:56:59 -0300

    feat: define basic signalling-related messages

--- webrtc_baseTypes.hpp
+++ webrtc_baseTypes.hpp
@@ -1,15 +1,29 @@
 #ifndef webrtc_base_TYPES_HPP
 #define webrtc_base_TYPES_HPP
 
-/* If you need to define types specific to your oroGen components, define them
- * here. Required headers must be included explicitly
- *
- * However, it is common that you will only import types from your library, in
- * which case you do not need this file
- */
+#include <string>
 
 namespace webrtc_base {
+    /** Different types of signalling messages */
+    enum SignallingMessageType {
+        SIGNALLING_NEW_PEER,          //! a new peer has connected
+        SIGNALLING_PEER_DISCONNECT,   //! a peer is disconnecting itself
+        SIGNALLING_PEER_DISCONNECTED, //! a peer has been disconnected
+        SIGNALLING_REQUEST_OFFER,     //! a polite peer is requesting an offer
+        SIGNALLING_OFFER,             //! offer from a peer
+        SIGNALLING_ANSWER,            //! answer from a peer
+        SIGNALLING_ICE_CANDIDATE      //! ICE negotiation message
+    };
+
+    /** A message from the signalling protocol */
+    struct SignallingMessage {
+        std::string from;
+        std::string to;
+        SignallingMessageType type;
+        std::string message;
+        /** m-line index in case type is ICE_CANDIDATE */
+        unsigned int m_line = 0;
+    };
 }
 
 #endif
-
